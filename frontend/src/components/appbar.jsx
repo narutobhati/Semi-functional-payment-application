af8ef1ad790bccc2ff664ef9beb8be1593b4844f
narutobhati <127118340+narutobhati@users.noreply.github.com>
2024-08-01 01:01:45 +0530

    Update appbar.jsx

--- frontend/src/components/appbar.jsx
+++ frontend/src/components/appbar.jsx
@@ -6,10 +6,18 @@ export function Appbar(){
             <div className="flex mx-2 ">
                 <div className="font-sans text-xl py-2.5 mx-2 font-semibold">Hello </div>
                  <div className="flex justify-center flex-col mt-2 mr-5 ">
-                <Link to={'/signup'}>  <button onClick={()=>{
-                    localStorage.clear();
-                }}   type="button" className="  bg-slate-700 hover:bg-slate-800 focus:outline-none  font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 >
-                LogOut </button></Link>
+               <Link to={'/signup'}>
+  <button
+    onClick={() => {
+      localStorage.clear();
+    }}
+    type="button"
+    className="bg-slate-700 hover:bg-slate-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+  >
+    LogOut
+  </button>
+</Link>
+
                
             </div>
                 <div className="bg-slate-400 rounded-full py-2.5 px-4 text-xl font-sans font-bold m-1">S</div>
