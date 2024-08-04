77cbd5ed4bba65c6f0f9b70ccd067a9311fa4bea
narutobhati <127118340+narutobhati@users.noreply.github.com>
2024-08-04 12:59:01 +0530

    Update signin.jsx

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -11,7 +11,16 @@ export function Signin(){
                 <Subheading subheading={"Enter your Credentials to access your account"}></Subheading>
                 <Inputbox tittle={"Email"} placeholder={"xyz@email.com"} type={"email"}></Inputbox>
                 <Inputbox tittle={"Password"} type={"password"}></Inputbox>
-                <Button title={"Sign in"} ></Button>
+                 <Button onclick={async ()=>{const response= await axios.post("https://semi-functional-payment-application-1.onrender.com/api/v1/user/signin",{
+                    username:email,
+                    password:password
+                })
+                    localStorage.setItem("token",response.data.token)
+                    {if(response.data.flag===true)
+                        navigate('/dashboard')
+                    }
+                }
+                } title={"Sign in"}  ></Button>
                 <Bottomwarning content={"Don't have an Account?"} todo={"Sign up"} address={"https://semi-functional-payment-application-a4y1.vercel.app//signup"}></Bottomwarning>
             </div>
     </div>
